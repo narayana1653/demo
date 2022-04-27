07fcc8c85c80527c37a1afc86fa4170511044986
narayana1653 <86287691+narayana1653@users.noreply.github.com>
2022-04-27 19:17:47 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("rammy");
-		System.out.println("nari");
-		System.out.println("nari");
+		
 	}
 
 }
