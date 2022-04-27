5cdce92b3721200334496e3144027f440a45a381
narayana1653 <86287691+narayana1653@users.noreply.github.com>
2022-04-27 19:30:03 +0530

    Update DemoApplication.java
    nrmrc

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,12 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
-		
-		
-		
-		
+		System.out.println("1");
 		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		
 	}
 
 }
